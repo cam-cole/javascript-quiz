da3517a4123c73ad0ee697164ed34ee010075de8
cam-cole <camcole065@gmail.com>
2022-04-02 14:12:33 -0500

    started build quiz function to go through each question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,56 @@
 var timerEl = document.getElementById('timer');
+var quizContainer = document.getElementById('quiz-container')
+
+// Quiz Questions
+var quizQuestions = [
+{
+    question: "Commonly used data types DO NOT include:",
+    answers: {
+        a: "strings",
+        b: "booleans",
+        c: "alerts",
+        d: "numbers"
+    },
+    correctAnswer: "c"
+},
+{
+    question: "String values must be enclosed within ______ when being assigned to variables.",
+    answers: {
+        a: "commmas",
+        b: "curly brackets",
+        c: "quotes",
+        d: "parentheses"
+    },
+    correctAnswer: "c"
+}
+]
+
+// function to build quiz
+function buildQuiz() {
+    // variable to store HTML output
+    var output = [];
+    
+    // for each question
+    quizQuestions.forEach((currentQuestion, questionNumber) => {
+        // variable to store the list of possible answers
+        var answers = []
+
+        for(letter in currentQuestion.answers){
+            answers.push(
+                `<button class="btn btn-primary m-3">${letter}: ${currentQuestion.answers[letter]}</button>`
+            );
+        }
+
+        // add this question and its answers to the output
+        output.push(
+            `<div>${currentQuestion.question}</div>
+            <div>${answers.join('')}</div>`
+        );
+    })
+
+    quizContainer.innerHTML = output.join('');
+}
+
 
 // Timer function for the quiz
 function countdown() {
@@ -17,3 +69,4 @@ function countdown() {
 }
 
 $("#start").on("click", countdown);
+$("#start").on("click", buildQuiz);
