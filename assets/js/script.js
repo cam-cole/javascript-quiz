a072dd14dd460a63efb587b8fafb3a801b3cf778
cam-cole <camcole065@gmail.com>
2022-04-03 14:48:16 -0500

    added feature to remove 10 seconds if answer is incorrect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 var timerEl = document.getElementById('timer');
+var timeLeft = 60;
 var quizContainer = document.getElementById('quiz-container')
 var resultContainer = document.getElementById('result-container')
+var answerCorrect = document.createElement("h2");
+var answerIncorrect = document.createElement("h2");
+
+answerCorrect.textContent = "Correct!";
+answerIncorrect.textContent = "Incorrect. You lost 10 seconds";
 
 // Quiz Questions
 var quizQuestions = [
@@ -60,51 +66,64 @@ function startQuiz() {
     quizContainer.appendChild(question);
     quizContainer.appendChild(answerList);
 
+    // JQuery events to check for right answer
     $("#a").on("click", function() {
         console.log(this.id);
         if(this.id === quizQuestions[0].correctAnswer) {
             console.log("You are right!");
+            resultContainer.appendChild(answerCorrect);
         }
 
         else {
             console.log("You are wrong!");
+            resultContainer.appendChild(answerIncorrect);
+            timeLeft = timeLeft - 10;
         }
     });
     $("#b").on("click", function() {
         console.log(this.id);
         if(this.id === quizQuestions[0].correctAnswer) {
             console.log("You are right!");
+            resultContainer.appendChild(answerCorrect);
         }
 
         else {
             console.log("You are wrong!");
+            resultContainer.appendChild(answerIncorrect);
+            timeLeft = timeLeft - 10;
         }
     });
     $("#c").on("click", function() {
         console.log(this.id);
         if(this.id === quizQuestions[0].correctAnswer) {
             console.log("You are right!");
+            resultContainer.appendChild(answerCorrect);
         }
 
         else {
             console.log("You are wrong!");
+            resultContainer.appendChild(answerIncorrect);
+            timeLeft = timeLeft - 10;
         }
     });
     $("#d").on("click", function() {
         console.log(this.id);
         if(this.id === quizQuestions[0].correctAnswer) {
             console.log("You are right!");
+            resultContainer.appendChild(answerCorrect);
         }
 
         else {
             console.log("You are wrong!");
+            resultContainer.appendChild(answerIncorrect);
+            timeLeft = timeLeft - 10;
         }
     });
 }
 
 // Timer function for the quiz
 function countdown() {
-    var timeLeft = 60;
+    // var timeLeft = 60;
 
     var timeInterval = setInterval(function() {
         if (timeLeft > 0) {
